dbf9c48e9f332e404ab17da64aa1967fbbc43fdf
tpouler <tpouler@middlebury.edu>
2023-05-09 15:40:05 +0200

    Create Reporter

--- app/src/main/java/com/example/newsroom/ui/screen/signup/SignUpViewModel.kt
+++ app/src/main/java/com/example/newsroom/ui/screen/signup/SignUpViewModel.kt
@@ -4,8 +4,13 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import com.example.newsroom.data.Post
+import com.example.newsroom.data.Reporter
+import com.example.newsroom.ui.screen.writepost.WritePostUiState
+import com.example.newsroom.ui.screen.writepost.WritePostViewModel
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.delay
 
@@ -17,11 +22,33 @@ sealed interface RegisterUIState {
 }
 
 class RegisterViewModel(): ViewModel() {
+    companion object {
+        const val COLLECTION_REPORTERS = "reporters"
+    }
+
     var registerUIState: RegisterUIState by mutableStateOf(RegisterUIState.Init)
     private lateinit var auth: FirebaseAuth
 
     init {
         auth = Firebase.auth
+    }
+
+    fun createReporter(
+    ) {
+        registerUIState = RegisterUIState.Loading
+
+        val myReporter = Reporter(
+            uid = auth.currentUser!!.uid,
+            author = auth.currentUser!!.email!!,
+        )
+
+        val reporterCollection = FirebaseFirestore.getInstance().collection(COLLECTION_REPORTERS)
+
+        reporterCollection.add(myReporter).addOnSuccessListener {
+            registerUIState = RegisterUIState.RegisterSuccess
+        }.addOnFailureListener{
+            registerUIState = RegisterUIState.Error(it.message)
+        }
     }
 
     suspend fun registerUser(email: String, password: String) {
